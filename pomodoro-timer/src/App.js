07ee8d4ad7e5c289b4f728ac80486e53634dfadb
ted-palmer <etpalmer17@gmail.com>
2021-04-29 15:28:18 -0400

    Installed styled-components and added basic styling

--- pomodoro-timer/src/App.js
+++ pomodoro-timer/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
-import Tasks from './components/Tasks/Tasks';
+import "./App.css";
+import styled from "styled-components";
+
+import Tasks from "./components/Tasks/Tasks";
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  background-color: #2ea9df;
+  background-image: url(./assets/texture.png);
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Tasks/>
-    </div>
+    <Background>
+      <Container>
+        <Tasks />
+      </Container>
+    </Background>
   );
 }
 

86206496e9b965be58443edc5018d2243983df30
ted-palmer <etpalmer17@gmail.com>
2021-04-29 15:13:10 -0400

    Setup basic file structure

---
+++ pomodoro-timer/src/components/Tasks/Tasks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Tasks = () => {
+
+}
+
+return(
+    <div>
+        
+    </div>
+)
+
+export default Tasks;
---
+++ pomodoro-timer/src/components/Timer/Timer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Timer = () => {
+
+
+}
+
+return(
+    <div>
+        
+    </div>
+)
+
+
+export default Timer;

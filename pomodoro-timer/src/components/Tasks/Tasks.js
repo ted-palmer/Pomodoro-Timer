1d617ebaa5bc9468d8c078715196333830a37674
ted-palmer <etpalmer17@gmail.com>
2021-04-29 15:22:50 -0400

    Built a basic add task form

--- pomodoro-timer/src/components/Tasks/Tasks.js
+++ pomodoro-timer/src/components/Tasks/Tasks.js
@@ -1,14 +1,31 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 const Tasks = () => {
+  const [value, setValue] = useState("");
+  const [currentTask, setCurrentTask] = useState("");
 
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCurrentTask(value);
+    setValue("");
+  };
 
-return(
+  return (
     <div>
-        
+      Current Task: {currentTask}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Enter in a task"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <br />
+        <button>Add Task</button>
+      </form>
     </div>
-)
+  );
+};
 
 export default Tasks;
